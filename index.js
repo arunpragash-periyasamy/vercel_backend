5a281b8fe6a9b1c1355c1dc59ba40ce671d8224d
arunpragash-periyasamy <arunpragash46@gmail.com>
2024-06-14 15:29:47 +0530

    Developed a basic node application

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const serverless = require("serverless-http");
+const router = express.router();
+
+router.get("/", (req, res) => {
+    res.send("Hello world from node js");
+});
+
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
